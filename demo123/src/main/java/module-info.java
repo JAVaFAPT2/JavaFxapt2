32eb81ab35423598c6b09d7409399a2c46db3d63
JAVaFAPT2 <dangqwe321@gmail.com>
2023-10-11 17:30:26 +0700

    module fix

--- demo123/src/main/java/module-info.java
+++ demo123/src/main/java/module-info.java
@@ -1,14 +1,14 @@
 /**
  *
  */
-module org.example {
+module demo123.src.main.java.view {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires org.jetbrains.annotations;
 
-
-
+    exports view;
     exports controller;
     opens controller to javafx.fxml;
 }
+
